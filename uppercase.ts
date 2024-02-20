5bdf5cbf5f0a53fe33dff7ca3224525ae9e725ac
ZUBAIR ALI <160566081+Zubair671@users.noreply.github.com>
2024-02-20 02:45:57 -0800

    Add files via upload

---
+++ uppercase.ts
@@ -0,0 +1,8 @@
+//  a="zubair"
+// let b="panhwer"
+// // a=length.toLowerCase;
+// // b=length.toUpperCase;
+// console.log(a) 
+ let uname =     "zubair";
+console.log("       ",   uname.toUpperCase());
+console.log("       ",   uname.toLowerCase());
