23a2bbd72f29a9b2476aaf71a74487f694e2d935
ZUBAIR ALI <160566081+Zubair671@users.noreply.github.com>
2024-02-20 04:57:45 -0800

    Add files via upload

---
+++ divdie mutipiltion 8number.ts
@@ -0,0 +1,5 @@
+// 7th assigment 8th assgiment
+console.log(5+3)
+console.log(11-3)
+console.log(64/8)
+console.log(4*2)
---
+++ famous_person.ts
@@ -0,0 +1,10 @@
+let quote : string = "a persson hwho never misatke tried";
+  let author:string ="albert einsten ";
+  console.log(`${author} once said,"${quote}"`)
+
+  
+// assigment no 05
+
+  let famous_person:string ="albert einsten";
+let message : string =`${famous_person}once said"${quote}"`;
+console.log(message)
